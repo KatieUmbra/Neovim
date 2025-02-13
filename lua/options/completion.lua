a0f33314cfddb600633fedae5b305c03334b1b0f
Katherine <katherine@kaytea.dev>
2025-02-12 20:51:40 -0500

    todo: fixed license year

--- lua/options/completion.lua
+++ lua/options/completion.lua
@@ -1,6 +1,4 @@
-local option = vim.opt
-local options = vim.opts
 local global = vim.g
 
 global.completeopt = { "menu", "menuone", "noselect", "noinsert" }
-vim.cmd([[ g:completion_enable_auto_popup = 1 ]])
+global.completion_enable_auto_popup = 1
